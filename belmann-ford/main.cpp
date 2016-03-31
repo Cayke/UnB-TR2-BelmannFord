fe9f33e6b6f6286b34bbb991910f0d264dfdf1a2
Igor Miranda <igormiranda5@gmail.com>
2016-03-30 21:39:24 -0300

    V1.0

--- belmann-ford/main.cpp
+++ belmann-ford/main.cpp
@@ -1,3 +1,9 @@
+/**
+ * 
+ * Igor Fernandes Miranda 11/0013255 
+ * Cayke Gabriel dos Santos Prudente 11/0112491
+ */
+
 #include "GraphFuncs.h"
 #include "BellmanFord.h"
 #include <cstdlib>
@@ -6,21 +12,30 @@
 
 int main(int argc, char** argv) {
     
-    std::string fileName;
-    int source, dest;
+    std::string fileName = "test.txt";
+    int source = 1, dest = 4, op = 2;
+    
     std::cout << "Digite nome do arquivo de entrada\n";
     std::cin >> fileName;
-    std::cout << "\nPartida\n";
-    std::cin >> source;
-    std::cout << "\nDestino\n";
-    std::cin >> dest;
-    
     std::vector<GraphNode> graph = assembleGraph(fileName);
-   
-    if(source == dest){
-        BellmanFord::printMatrix(graph);
-    }else{
-        BellmanFord::printPath(graph, source, dest);
+            
+    std::cout << "1->Caminho de fonte ao destino\n";
+    std::cout << "2->Matriz custo\n";
+    std::cin >> op;
+    
+    switch(op){
+        case 1:
+            std::cout << "\nPartida\n";
+            std::cin >> source;
+            std::cout << "\nDestino\n";
+            std::cin >> dest;
+            BellmanFord::printPath(graph, source, dest);
+            break;
+        case 2:
+            BellmanFord::printMatrix(graph);
+            break;
+        default:
+            BellmanFord::printMatrix(graph);
     }
     //printGraph(graph);
     
